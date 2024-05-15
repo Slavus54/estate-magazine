a04f996cdc3e38fc465f555a218cf6f8ba266f3d
Slavus54 <x.mirosfromsibir@gmail.com>
2024-05-15 23:49:09 +0700

    Fixed some bugs

--- src/components/Property.tsx
+++ src/components/Property.tsx
@@ -4,12 +4,12 @@ import {type PropertyPageProps} from '../env/types'
 const Property = ({property}: PropertyPageProps) => {
     return (
         <>
-            <image src={property.image} class='photo' />
+            <img src={property.image} className='photo' />
             <h2>{property.title}</h2>
 
-            <div class='items small'>
-                <h4 class='pale'>Type: {property.category}</h4>
-                <h4 class='pale'>Cost: {property.cost}₽</h4>
+            <div className='items small'>
+                <h4 className='pale'>Type: {property.category}</h4>
+                <h4 className='pale'>Cost: {property.cost}₽</h4>
             </div>
 
             <button><a href='/properties'>Back</a></button>
